1dad334569050ff525a0320323729ca3e0d95b4d
Samuel Libardi Godoy <samlibardi@outlook.com>
2019-06-08 20:37:03 -0300

    Parser now collects more info

--- py/parse_usp_pos.py
+++ py/parse_usp_pos.py
@@ -57,16 +57,16 @@ async def main():
 	links_unidades = soup.find_all('a', href=re.compile("publico/lista/disciplina"))
 
 	# Popular o dicionário de unidades a partir dos links encontrados
-	global codigos_unidades
-	codigos_unidades = {key:value for (key, value) in map(lambda x: (x.string, re.search("publico/lista/disciplina/(\d+)", x.get('href')).group(1)), links_unidades)}
+	global unidades
+	unidades = [{'nome': nome, 'codigo':cod} for (nome, cod) in map(lambda x: (next(x.stripped_strings,''), re.search("publico/lista/disciplina/(\d+)", x.get('href')).group(1)), links_unidades)]
 	
-	logger.info(" - %d unidades de ensino encontradas - " % (len(codigos_unidades)))
+	logger.info(" - %d unidades de ensino encontradas - " % (len(unidades)))
 
 	# Iniciar a iteração das unidades de acordo com as unidades encontradas ou fornecidas por argumento opcional, de forma assíncrona.
 	if not args.unidades:
-		materias = await iterar_unidades(codigos_unidades.values())
+		materias = await iterar_unidades(unidades)
 	else:
-		materias = await iterar_unidades(args.unidades)
+		materias = await iterar_unidades(list(filter(lambda x: x['codigo'] in args.unidades, unidades)))
 
 	# Salvar em arquivo json
 	materias_json = json.dumps(materias)
@@ -86,10 +86,10 @@ async def main():
 	logger.info(f" - \n - Tempo de execução: {time.perf_counter() - t} segundos")
 	return 0
 
-async def iterar_unidades(codigos_unidades):
+async def iterar_unidades(unidades):
 	#Chamar todas as unidades simultaneamente, de forma assíncrona
 	logger.info(" - Iniciando processamento de unidades")
-	unidade_tasks = (await asyncio.wait([iterar_unidade(i) for i in codigos_unidades]))[0]
+	unidade_tasks = (await asyncio.wait([iterar_unidade(u) for u in unidades]))[0]
 
 	logger.info(f" -   {len(unidade_tasks)} unidades processadas")
 	logger.info(" - Iniciando processamento de materias")
@@ -109,26 +109,34 @@ async def iterar_unidades(codigos_unidades):
 	logger.info(f" -   {len(materias_tasks)} materias processadas")
 	return [i.result() for i in materias_tasks if i.result()]
 
-async def iterar_unidade(codigo):
-	logger.debug(" -    Obtendo as materias da unidade %s - " % (codigo))
-	response = await session.get('https://uspdigital.usp.br/janus/AreaListaPublico?tipo=T&codcpg=' + codigo, timeout = 120)
+async def iterar_unidade(unidade):
+	logger.debug(f" -    Obtendo as materias da unidade {unidade}")
+	response = await session.get('https://uspdigital.usp.br/janus/AreaListaPublico?tipo=T&codcpg=' + unidade['codigo'], timeout = 120)
 	assert response.status == 200
 	soup = BeautifulSoup(await response.text(), "html5lib")
 	
-	links_materias = []
+	materias = []
 
-	links_areas = soup.find_all('a', href=re.compile("publico/lista/turma"))
-	codigos_areas = {key:value for (key, value) in map(lambda x: (x.string, re.search("publico/lista/turma/\d+/(\d+)", x.get('href')).group(1)), links_areas)}
+	tabelas_departamentos = soup.find_all('table')
+	departamentos = map(extrai_departamento, tabelas_departamentos)
+
+	#links_areas = soup.find_all('a', href=re.compile("publico/lista/turma"))
+	#codigos_areas = {key:value for (key, value) in map(lambda x: (x.string, re.search("publico/lista/turma/\d+/(\d+)", x.get('href')).group(1)), links_areas)}
 	
-	for area in codigos_areas.values():
-		logger.debug(f' -		Obtendo as materias da area de concentração {area}')
-		response = await session.get('https://uspdigital.usp.br/janus/TurmaLista?codare=' + area, timeout = 120)
-		assert response.status == 200
-		soup = BeautifulSoup(await response.text(), "html5lib")
-		links_materias = links_materias + soup.find_all('a', href=re.compile('publico/turma'))
+	for departamento in departamentos:
+		for area in departamento['areas']:
+			logger.debug(f' -		Obtendo as materias da area de concentração {area}')
+			response = await session.get('https://uspdigital.usp.br/janus/TurmaLista?codare=' + area['codigo'], timeout = 120)
+			assert response.status == 200
+			soup = BeautifulSoup(await response.text(), "html5lib").find('body')
+			materias_area = [*map(extrai_materia, soup.find_all('a', href=re.compile('publico/turma')))]
+			for materia in materias_area:
+				materia['departamento'] = departamento['nome']
+				materia['area_concentracao'] = area['nome']
+				materia['unidade'] = unidade['nome']
+			materias = materias + materias_area
 
-	materias = list(map(extrai_materia, links_materias))
-	logger.debug(f" -   {len(materias)} materias encontradas na unidade {codigo} - ")
+	logger.debug(f" -   {len(materias)} materias encontradas na unidade {unidade} - ")
 
 	return materias # Retorna uma lista de matérias para serem buscadas
 
@@ -141,40 +149,42 @@ async def parsear_materia(materia):
 		return
 
 	async with semaforo: # Semaforo controla o número de chamadas simultâneas
-		logger.debug(f" -      Obtendo turmas de {materia[0]} - {materia[1]}")
-		codigo = materia[0]
-		codtur = materia[1]
+		logger.debug(f" -      Obtendo turmas de {materia['codigo']}")
+		codigo = materia['codigo']
+		codtur = materia['turma']
 		try:
 			response = await session.get(f'https://uspdigital.usp.br/janus/TurmaDet?sgldis={codigo}&ofe={codtur}', timeout=args.timeout, verify_ssl=False)
 			assert response.status == 200
 			response = await response.text()
 		except asyncio.TimeoutError: # Tentar acessar o janus novamente, caso o pedido falhe.
 			try:
-				logger.warn(f" -      O pedido de turmas de {codigo} excedeu o tempo limite do pedido. Tentando novamente...")
+				logger.warn(f" -      O pedido de turmas de {materia['codigo']} excedeu o tempo limite do pedido. Tentando novamente...")
 				response = await session.get(f'https://uspdigital.usp.br/janus/TurmaDet?sgldis={codigo}&ofe={codtur}', timeout=args.timeout*2, verify_ssl=False)
 				assert response.status == 200
 				response = await response.text()
 			except asyncio.TimeoutError:
-				logger.error(f" -      O pedido de turmas de {codigo} excedeu o tempo limite do pedido")
+				logger.error(f" -      O pedido de turmas de {materia['codigo']} excedeu o tempo limite do pedido")
 				return
 		except:
-			logger.exception(f" -      Não foi possível obter turmas de {materia[0]} - {materia[1]}")
+			logger.exception(f" -      Não foi possível obter turmas de {materia['codigo']}")
 			return
 	
-		logger.debug(f" -      Analisando turmas de {materia[0]} - {materia[1]}")
+		del materia['turma']
+
+		logger.debug(f" -      Analisando turmas de {materia['codigo']}")
 		soup = BeautifulSoup(response, "html5lib")
 		tabela = soup.find('table')
 		try:
 			info = parsear_turma(tabela) #Obter informações das turmas
 		except:
-			logger.exception(f" -     Não foi possível parsear turmas de {materia[0]} - {materia[1]}")
+			logger.exception(f" -     Não foi possível parsear turmas de {materia['codigo']} - {materia['nome']}")
 			return
 
-		materia = info
+		materia.update(info)
 
 		# Salvar em .json e retornar
 
-		logger.debug(f" -      Salvando {codigo}")
+		logger.debug(f" -      Salvando {materia['codigo']} - {materia['nome']}")
 
 		materia_json = json.dumps(materia)
 
@@ -196,7 +206,7 @@ async def parsear_materia(materia):
 # Obtém as informações da matéria e retorna um dicionario da forma 
 # {unidade: "", departamento: "", campus "", objetivos: "", programa_resumido: "", creditos_aula: "", creditos trabalho: ""}
 def parsear_info_materia(tabelas_folha):
-	info = {}
+	info = {'grau': 'Pós-Graduação'}
 
 	re_nome = re.compile("Disciplina:\s+.{7}\s+-.+")
 	re_creditos = re.compile("Créditos\s+Aula")
@@ -225,7 +235,7 @@ def parsear_info_materia(tabelas_folha):
 def parsear_turma(tabela):
 	info = {}
 	turma = {}
-	professores = vagas = horario = None
+	professores = ''
 
 	trs = tabela.find('tbody').find_all('tr', recursive = False)
 	
@@ -233,7 +243,7 @@ def parsear_turma(tabela):
 	
 	info['codigo'] = codsearch.group(1)
 	turma['codigo'] = codsearch.group(2)
-	info['nome'] = ''.join(trs[1].stripped_strings)
+	info['nome'] = ''.join(trs[1].stripped_strings).replace('\u0096','-') #fix para erro de decoding de latin1 pelo aiohttp
 
 	for tr in trs:
 		if tr.find(class_="intl_numeroVagas"):
@@ -244,10 +254,14 @@ def parsear_turma(tabela):
 			turma['inicio'] = list(tr.stripped_strings)[1]
 			turma['fim'] = list(tr.stripped_strings)[3]
 		elif tr.find(class_="intl_docenteMinistrante"):
-			turma['professores'] = [''.join(x.stripped_strings) for x in tr.find_all('tr')][1:]
+			professores = [''.join(x.stripped_strings) for x in tr.find_all('tr')][1:]
 		elif tr.find(class_="intl_horario"):
 			turma['horario'] = parsear_horario(tr.find_next_sibling().find('table'))
 	
+	if turma['horario']:
+		for hor in turma['horario']:
+			hor['professores'] = professores
+
 	info['turmas'] = [turma]
 	return info
 
@@ -296,9 +310,16 @@ def parsear_vagas(tabela):
 	tr = tabela.find_all('tr')[1]
 	tds = tr.find_all('td')
 
-	vagas = {
-		'regular': int(tds[0].string if (tds[0].string != '-') else tds[2].string), 
-		'especial': int(tds[1].string if (tds[1].string != '-') else 0)
+	vreg = int(tds[0].string if (tds[0].string != '-') else tds[2].string)
+	vesp = int(tds[1].string if (tds[1].string != '-') else 0)
+
+	vagas = { 'Pós-Graduação': {
+			'vagas': vreg + vesp,
+			'grupos':{
+				'Alunos Regulares': {'vagas': vreg},
+				'Alunos Especiais': {'vagas': vesp}
+				}
+			}
 		}
 
 	return vagas
@@ -345,10 +366,25 @@ def parsear_horario(tabela):
 #Retorna um par (codigo, turma), exemplo: (u'PRO5961', u'2')
 def extrai_materia(x):
 	search = re.search("publico/turma/([A-Z0-9\s]{7})/(\d+)", x.get('href'))
-	return (search.group(1), search.group(2)) if search else None
+	return {'codigo': search.group(1), 'turma': search.group(2)} if search else None
 
 # ----------------------
 
+def extrai_departamento(tabela):
+	info = {}
+	cabeçalho = list(tabela.find('thead').stripped_strings)
+	info['codigo'] = cabeçalho[0]
+	info['nome'] = cabeçalho[1]
+	info['areas'] = []
+
+	trs = tabela.find('tbody').find_all('tr', recursive = False)
+	areare = re.compile('(\d+)\s+-\s+(.+)')
+	for tr in trs:
+		search = areare.search(next(tr.stripped_strings,''))
+		areainfo = {'codigo': search.group(1), 'nome': search.group(2)}
+		info['areas'].append(areainfo)
+
+	return info
 		
 #Execução do programa
 if __name__ == "__main__":
@@ -383,7 +419,7 @@ if __name__ == "__main__":
 	logger.addHandler(ch)
 
 	# Enviar log para arquivo
-	fh = logging.FileHandler(time.strftime('%Y-%m-%d_%H-%M-%S_'+os.path.basename(__file__)+'.log'))
+	fh = logging.FileHandler(time.strftime('%Y-%m-%d_%H-%M-%S_'+os.path.basename(__file__)+'.log'), encoding='utf-8')
 	fh.setLevel(logging.DEBUG)
 	fh.setFormatter(logging.Formatter('[%(asctime)s] %(module)s %(levelname)s: %(message)s'))
 	logger.addHandler(fh)
